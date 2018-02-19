50d0cfea3d578371cee006c49d3cf7379ddede49
braerjustin <35747682+braerjustin@users.noreply.github.com>
2018-02-18 21:57:02 -0500

    Update footer.php

--- _templates/_footers/footer.php
+++ _templates/_footers/footer.php
@@ -8,6 +8,8 @@
 			<div class="row">
 				<div class="col-sm-6 text-center">478.471.2700 | <a href="https://www.google.com/maps/dir/100 university parkway macon ga 31206" style="color: #FFFFFF">100 University Parkway | Macon, GA 31206</a></div>
 				<div class="col-sm-6 text-center">
+					<a href="https://www.mga.edu/technology/services/email.php"> <img src="<?php echo $server.'/_images/email_icon.jpg';?>" height="30" width="30" alt="e"></a>
+					<a href="https://www.mga.edu/d2l/index.php"> <img src="<?php echo $server.'/_images/MGA_Inst_Vert.jpg';?>" height="30" width="30" alt="d"></a>
 					<a href="https://www.facebook.com/MiddleGeorgiaStateUniversity2"> <img src="<?php echo $server.'/_images/facebook.jpg';?>" height="30" width="30" alt="f"></a>
 					<a href="https://twitter.com/MGAstateU"> <img src="<?php echo $server.'/_images/twitter.jpg';?>" height="30" width="30" alt="t"</a>
 					<a href="https://www.instagram.com/mgastateu/"> <img src="<?php echo $server.'/_images/instagram.jpg';?>" height="30" width="30" alt="i"</a>
