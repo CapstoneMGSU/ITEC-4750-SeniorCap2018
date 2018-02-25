fdfe45882671402f9301a850f4a0bb320c06a8a2
srujanachandu <srujanachandupatla1@gmail.com>
2018-02-25 09:24:30 -0600

    Update facultyNav.php

--- _templates/_nav/facultyNav.php
+++ _templates/_nav/facultyNav.php
@@ -2,7 +2,7 @@
 	<!-- Change: Updated facultyFeedback address 11/18 KM -->
 	<!-- Sets up data for the side navigation bar. -->
 	<div id="mySidenav" class="sidenav">
-		<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+		<!--<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
 		<a href="facultyDashboard.php">Home</a>
 		<a href="all_classes.php">All Classes</a>		
 		<a href="classes.php">My Classes</a>
@@ -11,9 +11,21 @@
 		<a href="mystudents.php">My Students</a>
 		<a href="add_students.php">Upload Students</a>
 		<!-- <a href="createsurveys.php">Create New Survey</a> -->
-		<a href="settings.php">Settings</a>
+		<!--<a href="settings.php">Settings</a>
 		<a href="facultyFeedback.php">Feedback</a>
 		<a href="yoursurveys.php">Your Surveys</a>
+		<a href="../_php/logout.php">Logout</a>-->
+		
+	 	<!--Modified faculty nav-->
+		<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+		<a href="#">Menu</a>
+		<a href="facultyDashboard.php">Home</a>
+		<a href="all_classes.php">Courses</a>		
+		<a href="#">Group</a>
+		<a href="all_students.php">Students</a>
+		<!-- <a href="createsurveys.php">Create New Survey</a> -->
+		<a href="yoursurveys.php">Survey</a>
+		<a href="facultyFeedback.php">Feedback</a>
 		<a href="../_php/logout.php">Logout</a>
 	</div>
 	<div class="container-fluid">
