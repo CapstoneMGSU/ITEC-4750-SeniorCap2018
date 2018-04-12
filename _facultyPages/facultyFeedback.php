777df34c5b1e3895b984b802f186f22ede7823ee
fcheeley <35745104+fcheeley@users.noreply.github.com>
2018-04-12 03:05:55 -0400

    Update facultyFeedback.php
    load questions from database test

--- _facultyPages/facultyFeedback.php
+++ _facultyPages/facultyFeedback.php
@@ -3,24 +3,46 @@
 $title = 'Faculty Feed Back';
 include($_SERVER['DOCUMENT_ROOT'].'/_templates/_headers/facultyHeader.php');
 include($_SERVER['DOCUMENT_ROOT'].'/_templates/_nav/facultyNav.php');
+require($_SERVER['DOCUMENT_ROOT'].'/_php/_objects/drop_do.php');
 // ++++ Change: Added Page Identifier 10/10 KM ++++
 $P='facultyCourses';
 ?>
 
-
-
-<div class="wrapper">
+<html>
+<body>
+<div class="wrapperFeedback">
 <main>	
-	<div class="content">
+	<div class="feedback_table">
 				<div class="col-md-7 col-centered formatContainer">
 					<br>
-					<h4 class="center"><b>Place holder for Feed Back</b></h4>
-						<img src="../_images/feedback.png" alt="Place holder" />
+					<h4 class="center"><b>Survey Feedback</b></h4>
+						<table class="feeback_table">
+							<tr><th>Survey</th><th>Question</th><th>Total Replies</th></tr>
+							<tr>
+								<td><select>
+										<option value="0" selected>Select Question</option>
+					<?php 
+						$dropdo = new DROP_DO();
+						$rows=$dropdo->surveyQuestions();	// Populate selection from general survey data
+						foreach($rows AS $q){
+							echo ' <option value="'.$q['QuestionID'].'">'.$q['QuestionTxt'].'</option>';
+						}
+					?>
+									</select>
+								</td>
+								<td><select>
+										<option></option>
+									</select>
+								</td>
+								<td>"replies info here"</td>
+							</tr>
+						</table>
 					<br>
 				</div>
 	</div>
 </div>
 		<br>
+</main>
 <?php include($_SERVER['DOCUMENT_ROOT'].'/_templates/_footers/footer.php');?>
 	</body>
 </html>
