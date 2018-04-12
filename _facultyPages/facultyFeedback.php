2895e656083dfd835928bff4a2e47905ca6fabfa
braerjustin <35747682+braerjustin@users.noreply.github.com>
2018-04-12 02:04:01 -0400

    Update facultyFeedback.php

--- _facultyPages/facultyFeedback.php
+++ _facultyPages/facultyFeedback.php
@@ -7,20 +7,34 @@ include($_SERVER['DOCUMENT_ROOT'].'/_templates/_nav/facultyNav.php');
 $P='facultyCourses';
 ?>
 
-
-
-<div class="wrapper">
+<html>
+<body>
+<div class="wrapperFeedback">
 <main>	
-	<div class="content">
+	<div class="feedback_table">
 				<div class="col-md-7 col-centered formatContainer">
 					<br>
-					<h4 class="center"><b>Place holder for Feed Back</b></h4>
-						<img src="../_images/feedback.png" alt="Place holder" />
+					<h4 class="center"><b>Survey Feedback</b></h4>
+						<table class="feeback_table">
+							<tr><th>Survey</th><th>Question</th><th>Total Replies</th></tr>
+							<tr>
+								<td><select>
+										<option></option>
+									</select>
+								</td>
+								<td><select>
+										<option></option>
+									</select>
+								</td>
+								<td>"replies info here"</td>
+							</tr>
+						</table>
 					<br>
 				</div>
 	</div>
 </div>
 		<br>
+</main>
 <?php include($_SERVER['DOCUMENT_ROOT'].'/_templates/_footers/footer.php');?>
 	</body>
 </html>
