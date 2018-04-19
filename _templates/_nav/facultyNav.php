723e29b5241f4024f71444d15626111d3871b73f
fcheeley <35745104+fcheeley@users.noreply.github.com>
2018-04-19 18:31:04 -0400

    Update facultyNav.php
    removed redundant links

--- _templates/_nav/facultyNav.php
+++ _templates/_nav/facultyNav.php
@@ -4,13 +4,13 @@
 	<div id="mySidenav" class="sidenav">
 		<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
 		<a href="facultyDashboard.php">Home</a>
-		<a href="facultyCourses.php">Courses</a>
+		//<a href="facultyCourses.php">Courses</a>
 		<a href="facultyGroups.php">Groups</a>
 		<a href="facultyStudents.php">Students</a>
 			<!-- <a href="createsurveys.php">Create New Survey</a> -->
-		<a href="settings.php">Settings</a>
-		<a href="facultyFeedback.php">Feedback</a>
-		<a href="facultysurveys.php">Surveys</a>
+		//<a href="settings.php">Settings</a>
+		//<a href="facultyFeedback.php">Feedback</a>
+		//<a href="facultysurveys.php">Surveys</a>
 		<a href="../_php/logout.php">Logout</a>
 	</div>
 	<div class="container-fluid">
