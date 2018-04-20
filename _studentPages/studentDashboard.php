b77f3a7b4d4223fca63d5836659e223aa13c5790
fcheeley <35745104+fcheeley@users.noreply.github.com>
2018-04-20 15:08:48 -0400

    Update studentDashboard.php

--- _studentPages/studentDashboard.php
+++ _studentPages/studentDashboard.php
@@ -2,7 +2,7 @@
 // ++++ Change: Added Title 10/25 KM ++++
 //$title = 'Student Dashboard';
 include('../_templates/_headers/studentHeader.php');
-include('../_templates/_nav/studentNav.php');
+//include('../_templates/_nav/studentNav.php');
 include($_SERVER['DOCUMENT_ROOT'].'/_templates/_nav/getIDs.php');
 $P='studentDashboard';
 if($LoginID !=0){
@@ -13,7 +13,7 @@ if($LoginID !=0){
 	<!-- Main Content Section-->
 		<div id="main">
 			<?php // if(isset($_SESSION)){echo '<pre>'; print_r($_SESSION); echo '</pre>';}//error checking session	?>
-			<h2 class="center">Welcome <?php if(!empty($FName)){echo $FName . " " . $LName;}?></h2>
+			
 
 
 			<h2 class="center">Welcome <?php if(!empty($FName)){echo $FName . " " . $LName;}?></h2>
